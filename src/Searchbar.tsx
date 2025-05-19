338dbfeac92a3c2775b91e4407b75cf0b66ce626
armaaniel <ishmaan@hotmail.com>
2025-05-18 18:56:09 -0600

    searchbar

--- src/Searchbar.tsx
+++ src/Searchbar.tsx
@@ -3,110 +3,128 @@ import { gql, useLazyQuery } from '@apollo/client';
 import { useDebounce } from 'use-debounce';
 import { useNavigate } from 'react-router-dom';
 import './searchbar.css'
-
-
 const SEARCH_USERS = gql`
-
 query Search_Users($term:String!) {
-	usersByEmailOrName(term: $term) {
-		email
-		firstName	
-		id
-	}
-}
-`
+    usersByEmailOrName(term: $term) {
+        email
+        firstName    
+        id
+    }
+}`
 
 const Searchbar = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const [debouncedSearchTerm] = useDebounce(searchTerm, 150)
+
+    const [searchUsers, {loading, data}] = useLazyQuery(SEARCH_USERS, {
+
+        variables: {term: debouncedSearchTerm},
+        fetchPolicy: 'cache-and-network',
+
+    });
+
+    const navigate = useNavigate();
 	
-	const [searchTerm, setSearchTerm] = useState('');
 	
-	const [debouncedSearchTerm] = useDebounce(searchTerm, 250)
-	
-	const [searchUsers, {loading, data}] = useLazyQuery(SEARCH_USERS, {
+
+    const handleSearchChange = (e) => {
+
+        setSearchTerm(e.target.value);
 		
-		variables: {term: debouncedSearchTerm},
-		fetchPolicy: 'cache-and-network',
+		if (searchTerm === '') {
+		        setShowResults(false);
+		        setSearchResults([]);
+			}
+
+    };
 	
-	});
-	
-	const navigate = useNavigate();
-	
-	const searchResults = data?.usersByEmailOrName || [];
-	
-	const handleSearchChange = (e) => {
+    const [showResults, setShowResults] = useState(false);
+
+    const handleUserSelect = (user) => {
+
+        navigate(`/users/${user.id}`)
+        setSearchTerm('');
+
+    };
+
+    const [searchResults, setSearchResults] = useState([]);
+
+	useEffect(() => { 
+	  if (debouncedSearchTerm) {
+	    searchUsers();
+	  } else {
+	    setSearchResults([]);
+        setShowResults(false);
 		
-		setSearchTerm(e.target.value);
-	
-	};
-	
-	const handleUserSelect = (user) => {
-		
-		navigate(`/users/${user.id}`)
-		setSearchTerm('');
-	
-	};
-	
-	useEffect( () => { 
-	
-		if (debouncedSearchTerm) {
-			searchUsers();
-		}
+	  }
 	}, [debouncedSearchTerm, searchUsers]);
 	
+	useEffect(() => {
+	      if (data?.usersByEmailOrName) {
+	        setSearchResults(data.usersByEmailOrName);
+        
+	        const timer = setTimeout(() => {
+	          setShowResults(true);
+	        }, 250);
+        
+	        return () => clearTimeout(timer);
+	      }
+	    }, [data]);
+	
 
-	return (
-	
-	<>
-	
-	<div class='nav-search-div-two'>
-	
-	<div class='search-svg-div'>
-	
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="24" height="24">
-	      <path 
-	        d="m14 14-2.867-2.867m1.534-3.8A5.333 5.333 0 1 1 2 7.333a5.333 5.333 0 0 1 10.667 0Z" 
-	        stroke="#32302F" 
-	        stroke-width="1.8" 
-	        stroke-linecap="round" 
-	        stroke-linejoin="round"
-	        fill="none">
-	      </path>
-	    </svg>
-		
-	</div>
-	
-	<div class='search-parent'>
-	
+    return (
+
+    <>
+
+    <div class='nav-search-div-two'>
+
+    <div class='search-svg-div'>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="24" height="24">
+          <path 
+            d="m14 14-2.867-2.867m1.534-3.8A5.333 5.333 0 1 1 2 7.333a5.333 5.333 0 0 1 10.667 0Z" 
+            stroke="
+#32302F" 
+            stroke-width="1.8" 
+            stroke-linecap="round" 
+            stroke-linejoin="round"
+            fill="none">
+          </path>
+        </svg>
+
+    </div>
+
+    <div class='search-parent'>
+
     <input type='search' className='searchbar' placeholder='User Search' value={searchTerm} onChange={handleSearchChange} />
-	
+
   </div>
   </div>
-  
-    {debouncedSearchTerm && searchResults.length > 0 && (
-		<div className="search-results-container">
-		<h3 className='results-text'>results</h3>
-			<ul className="search-results">
-			{searchResults.map((user) => (
-				<li key={user.id} className="search-result-item" onClick={ () => handleUserSelect(user) }>
-				<div>{user.firstName}</div>
-				<div>{user.email}</div>
-		        </li>
+  <div className="search-results-container">
+
+  		{debouncedSearchTerm && showResults && (
+              <ul className="search-results">
+              {searchResults.map((user) => (
+                  <li key={user.id} className="search-result-item" onClick={ () => handleUserSelect(user) }>
+                  <div>{user.firstName}</div>
+                  <div>{user.email}</div>
+                  </li>
+
+                      ))}
+
+                    </ul>
+                )}
 				
-		            ))}
-					
-		          </ul>
-		        </div>
-		      )}
-			  
-		      {debouncedSearchTerm && searchResults.length === 0 && (
-		        <div className="search-results-container">
-				<h3 className='results-text'>results</h3>
-		          <div className="search-result-item">No users found</div>
-		        </div>
-		      )}}
-		    </>
-		  );
-		};
+				{debouncedSearchTerm && searchResults.length === 0 && showResults && (
+						          <div className="search-result-item">No users found</div>
+						      )}
+				
+                </div>
 
-		export default Searchbar;
-
+    
+              </>
+            );
+          };
+          export default Searchbar;
