092f370d9b6cf9da19165ee885b2178ad7aea66b
armaan <ishmaan@hotmail.com>
2025-05-20 23:17:09 -0600

    minor fix

--- src/views/Users.tsx
+++ src/views/Users.tsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { gql, useQuery, useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import ModifyUser from '../components/ModifyUser.tsx'
